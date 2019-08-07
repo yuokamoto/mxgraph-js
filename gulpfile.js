6d91ef3734dc4755f6efadfee0f31910c2f92d44
Yu Okamoto <yuokamoto1988@gmail.com>
2019-08-08 00:53:25 +0900

    update index and gupl

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+var ghpages = require('gh-pages');
+var path = require('path');
 var gulp = require('gulp');
 var usemin = require('gulp-usemin');
 var del = require('del');
@@ -6,33 +8,62 @@ var runSequence = require('run-sequence');
 var packageJson = require('./package');
 
 gulp.task('clean:dist', function(cb) {
-	return del(['dist'], cb);
+    return del(['dist'], cb);
 });
 
 gulp.task('usemin', function() {
-	return gulp.src('./index.html')
-		.pipe(usemin())
-		.pipe(gulp.dest('dist/'));
+    return gulp.src('./index.html')
+        .pipe(usemin())
+        .pipe(gulp.dest('dist/'));
+ 
 });
 
 gulp.task('webpack', function() {
-	return new Promise(function(resolve, reject) {
-		webpack({
-			entry: './dist/' + packageJson.name + '.js',  // mxGraphJS.js 该文件是gulp-usemin先产生的.主要用于拼接所有js文件
-			output: {
-				path: './dist/',
-				filename: packageJson.name + '.js',
-				library: 'mx',
-				libraryTarget: 'umd'
-			}
-		}).run(function() {
-			resolve();
-		});
-	});
+    return new Promise(function(resolve, reject) {
+        webpack({
+            entry: './dist/' + packageJson.name + '.js', // mxGraphJS.js 该文件是gulp-usemin先产生的.主要用于拼接所有js文件
+            output: {
+                path: './dist/',
+                filename: packageJson.name + '.js',
+                library: '',
+                libraryTarget: 'umd'
+            },
+            plugins: [
+                new webpack.optimize.UglifyJsPlugin({
+                    compress: {
+                        screw_ie8: true,
+                        warnings: false
+                    },
+                    mangle: {
+                        screw_ie8: true
+                    },
+                    output: {
+                        comments: false,
+                        screw_ie8: true
+                    }
+                })
+            ]
+        }).run(function() {
+            resolve();
+        });
+    });
 });
 
 gulp.task('dist', function() {
-	runSequence('clean:dist', 'usemin', 'webpack', function() {
-		console.log('dist success!');
-	});
+    runSequence('clean:dist', 'usemin', 'webpack', function() {
+        console.log('dist success!');
+    });
+});
+
+
+// deploy to gh-pages branch
+gulp.task('deploy', function() {
+    ghpages.publish(path.join(__dirname, './'), {
+        src: ['package.json', 'README.md', 'dist/*'],
+        logger: function(message) {
+            console.log(message);
+        }
+    }, function(err) {
+        console.log(err);
+    });
 });
